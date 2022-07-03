057930967c26165e862af95da267c4c1f6d3974a
Dilman Arif <dilmanarif01@gmail.com>
2022-07-03 22:48:14 +0300

    Update

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,12 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters-screen';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('filters'),
+      ),
     );
   }
 }
